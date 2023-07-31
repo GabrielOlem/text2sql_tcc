c2c96e9f5d176edb6761073e671fa445527d5979
Gabriel Melo <gme@cin.ufpe.br>
2023-07-30 21:05:06 -0300

    Inference update

--- inference.py
+++ inference.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     file_save = ""
     print('Inference Started')
     for query in database['NoAnswer']:
-        model_inputs = tokenizer(query, return_tensors="pt")
+        model_inputs = tokenizer(query, return_tensors="pt", return_token_type_ids=False)
         outputs = model.generate(**model_inputs, max_length=512)
         output_text = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
         file_save += output_text + '\n'
